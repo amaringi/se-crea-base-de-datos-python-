d35d145cab6c462e3c610ad0154e62a4c3e5b6f6
diseño y desarrollo <disenoydesarrollo@academicos.cesde.edu.co>
2024-09-17 20:45:48 -0500

    v01 feat v01 enlace base de datos

---
+++ app/database/configuration.py
@@ -0,0 +1,25 @@
+from sqlalchemy import create_engine,event
+from sqlalchemy.orm import sessionmaker 
+from sqlalchemy.engine import Engine
+
+
+
+
+#datos para la coneccion a base de datos 
+
+dataBaseName="gestor_de_venta"
+userName="root"
+userPassword=""
+cocentionPort=3306
+server="localhost"
+
+#Creando la conexion
+dataBaseConnection=f"mysql+mysqlconnector://{userName}:{userPassword}@{server}:{cocentionPort}/{dataBaseName}"
+
+engine=create_engine(dataBaseConnection)
+#abrir la session con la bd
+
+SessionLocal=sessionmaker(autocommit=False,autoflush=False, bind=engine)
+
+
+
---
+++ main.py
@@ -0,0 +1,6 @@
+
+nombre="juan carlos"
+edad=25
+
+print("mi nombre es ",nombre," y tengo ",edad," años")
+print(f"mi nombre es: {nombre} y tengo {edad} años")
